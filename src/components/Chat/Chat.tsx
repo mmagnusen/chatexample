b2b052ff12de6addb07192e7f27e9c197f1ca977
Marilyn Magnusen <m-mags@hotmail.co.uk>
2025-08-15 23:06:08 +0100

    webkit fill available

--- src/components/Chat/Chat.tsx
+++ src/components/Chat/Chat.tsx
@@ -48,7 +48,7 @@ const Chat = () => {
     }
 
     return (
-        <div style={{ background: '#dedeff', padding: '0 1rem', display: 'flex', flexDirection: "column", height: '100dvh' }}>
+        <div style={{ background: '#dedeff', padding: '0 1rem', display: 'flex', flexDirection: "column",  height: "-webkit-fill-available"}}>
             <div style={{
                 background: 'white',
                 padding: '1rem',
